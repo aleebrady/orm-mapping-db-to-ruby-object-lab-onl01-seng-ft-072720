f08efc303fc7c4b393a0528498c47463553c9cba
Aaron Brady <aleebrady@gmail.com>
2020-08-27 06:04:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,9 @@ class Student
   
   def self.all_students_in_grade_9
     sql <<-SQL
-    Select * From students Where grade = 9
+    Select * 
+    From students 
+    Where grade = 9
     SQL
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
