75a2e1784f9b3f1ae44d3a3c6cd0bb963f369827
Aaron Brady <aleebrady@gmail.com>
2020-08-27 06:09:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,7 +77,7 @@ class Student
     end
   end
     
-    def self.first_X_students_in_grade_10()
+    def self.first_X_students_in_grade_10(num)
       sql = <<-SQL
     Select * From students Where grade < 12
     SQL
